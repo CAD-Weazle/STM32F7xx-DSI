1c92a8694e9f704d93864647591e6f5b3d3d96c1
CAD-Weazle <cadweazle@proton.me>
2025-12-19 11:48:07 +0100

    Add files via upload

--- .github/main.c
+++ .github/main.c
@@ -3,13 +3,11 @@
 // purpose : STM32H769 DSI LCD bare metal
 // board   : STM32H769I-DISCO Eval Board
 // date    : 251203
-// last    : 251216
+// last    : 251219
 //
 // note    : STM32F769NIH6: 32-bit 2MB FLASH 512kB SRAM 216TFBGA
 
 #include "includes.h"
-
-int color = 0;
 
 int main (void)  
 {
@@ -20,12 +18,6 @@ int main (void)
   init_uart6    (115200);    // init UART6 - debug port via Arduino connector CN13 pin D0/D1
   init_dsi      ();          // setup DSI
   init_otm8009a ();          // init OTM8009A LCD Display IC Driver
-
-  // dump registers
-//dsi_reg_dump ();
-
-  // drawe first rectangle
-  fill_rect ();
 
   while (1)
   {
@@ -39,7 +31,8 @@ int main (void)
 
     if (sec_elapsed)
     {
-      led_red_toggle ();
+      // der Blinkenlichten
+      led_red_flash (10);
  
       sec_elapsed = 0;
     }
---
+++ .github/ticker.c
@@ -0,0 +1,100 @@
+// file    : ticker.c
+// author  : rb
+// purpose : STM32F7xx timer routines
+// date    : 251205
+// last    : 251204
+// note    : Timer 6 usleep timer
+
+#include "includes.h"
+
+// globals
+volatile int csec_elapsed = 0;
+volatile int dsec_elapsed = 0;
+volatile int  sec_elapsed = 0;
+
+// Timer 7 overflow interrupt
+void TIM7_IRQHandler (void)
+{
+  static int msec_cnt = 0;
+  static int csec_cnt = 0;
+  static int dsec_cnt = 0;
+
+  // clear interrupt (note: first thing to do in the ISR!!!!!)
+  TIM7->SR = 0;
+ 
+  msec_cnt++;
+
+  if (msec_cnt >= 10)
+  {
+    csec_elapsed = 1;
+
+    msec_cnt = 0;
+    csec_cnt++;
+  }
+
+  if (csec_cnt == 10)
+  {
+    dsec_elapsed = 1;
+
+    csec_cnt = 0;
+    dsec_cnt++;
+  }
+
+  if (dsec_cnt == 10)
+  {
+    sec_elapsed = 1;
+    dsec_cnt = 0;
+  }
+}        
+
+// setup Timers
+void init_ticker (void)
+{
+  // enable clock for Timers
+  RCC->APB1ENR |= RCC_APB1ENR_TIM6EN 
+               |  RCC_APB1ENR_TIM7EN;
+   
+  // init 16-bit Timer 6, freerunning at 1 MHz - 'usleep ()' 
+  TIM6->PSC    = TIMER_1MHZ;                // timer_clock
+  TIM6->EGR    = TIM_EGR_UG;                // force update
+  TIM6->CR1    = TIM_CR1_CEN;               // enable timer
+
+  // init 16-bit Timer 7, generate interrupt every ms - wall clock
+  TIM7->PSC    = TIMER_1MHZ;                // timer_clock
+  TIM7->ARR    = 1000;                      // set period
+  TIM7->EGR    = TIM_EGR_UG;                // force update
+  TIM7->DIER   = TIM_DIER_UIE;              // enable interrupts, disable DMA
+  TIM7->CR1    = TIM_CR1_CEN;               // enable timer
+  NVIC_EnableIRQ (TIM7_IRQn);               // enable interrupt Timer 7
+}
+
+// kill time with 1 us resolution
+void usleep (int16_t usecs)
+{
+  uint16_t start = TIM6->CNT + usecs;
+
+  while ((int16_t)(TIM6->CNT - start) < 0)
+    continue;
+}
+
+// kill time with ms resolution
+void msleep (uint16_t msecs)
+{
+  while (msecs--)
+    usleep (1000); 
+}
+
+// dump Timer registers -- dev only
+void timer_dump_registers (void)
+{
+//printf ("-- Timer 7 registers --\n");
+//printf ("TIM7->PSC  : %08lx\n", TIM7->PSC);
+//printf ("TIM7->ARR  : %08lx\n", TIM7->ARR);
+//printf ("TIM7->EGR  : %08lx\n", TIM7->EGR);
+//printf ("TIM7->CR1  : %08lx\n", TIM7->CR1);
+//printf ("TIM7->CR2  : %08lx\n", TIM7->CR2);  
+//printf ("TIM7->SMCR : %08lx\n", TIM7->SMCR);  
+//printf ("TIM7->CCMR1: %08lx\n", TIM7->CCMR1);  
+//printf ("TIM7->CCMR2: %08lx\n", TIM7->CCMR2);
+//printf ("TIM7->CCER : %08lx\n", TIM7->CCER);	
+}
